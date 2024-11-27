300c9fa9c11e68a441f6372dd5cde456419b7915
Nitesh Sharma <niteshsharma21307665@gmail.com>
2024-11-27 08:57:46 +0530

    Error and responce of API

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js"
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_url}/${DB_NAME}`)
-        console.log(`MongoDB connected on || Host ${connectionInstance}`)
+        console.log(`MongoDB connected on || Host ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log(`monngoodb is connection is failed ${error}`)
         process.exit(1)
